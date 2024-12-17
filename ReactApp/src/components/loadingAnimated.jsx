24ed4828d0f2d16858239390b22dc7c34a3025d2
CReid13524 <christian.reid007@gmail.com>
2024-12-18 11:07:32 +1300

    Loading Animation created

---
+++ ReactApp/src/components/loadingAnimated.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LoadingAnimated() {
+  return (
+    <div className='loading-container'>
+        <div className="loading-circle"></div>
+    </div>
+  )
+}
+
+export default LoadingAnimated
