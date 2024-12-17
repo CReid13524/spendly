b550abe27cc0b7ff6f33fa55e7b2c9a7f1fe058d
CReid13524 <christian.reid007@gmail.com>
2024-12-18 11:07:54 +1300

    Authentication sytems installed across app

--- ReactApp/src/App.jsx
+++ ReactApp/src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Routes, Route, Link, useLocation } from 'react-router-dom'
+import React, { useEffect, useRef, useState } from 'react'
+import { Routes, Route, Link, useLocation, useNavigate } from 'react-router-dom'
 
 // Example components
 import Login from './pages/login'
@@ -9,6 +9,7 @@ import Reconcile from './pages/reconcile'
 import Statistics from './pages/statistics'
 import Settings from './pages/settings'
 import NotFound from './pages/notFound'
+import LoadingAnimated from './components/loadingAnimated'
 
 function Navigation() {
   return (
@@ -26,8 +27,59 @@ function Navigation() {
 }
 
 function App() {
+  const [isAuthVerified, setIsAuthVerified] = useState(false);
   const location = useLocation();
   const hideNav = location.pathname === '/login';
+  const navigate = useNavigate();
+  const hasCheckedAuth = useRef(false);
+
+  useEffect(() => {
+    let timeoutId;
+    let intervalId;
+    // Run auth verification only if not on the login page
+    if (location.pathname !== '/login') {
+      const verifyAuth = async () => {
+        try {
+          setIsAuthVerified(false)
+          const response = await fetch('/api/secure', {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            credentials: 'include',
+          });
+          if (response.status == 500) {
+            throw response.statusText
+          }
+          const data = await response.json();
+          if (!data.valid) {
+            alert(`Session expired. Please log in again.\n(${data.error})`);
+            navigate('/login');
+          } else {
+            setIsAuthVerified(true);
+          }
+        } catch (error) {
+          alert(`Failed to verify authentication: ${error}`);
+          navigate('/login');
+        }
+      };
+
+      if (hasCheckedAuth.current) {
+      verifyAuth();
+      }
+      hasCheckedAuth.current = true
+
+      const intervalId = setInterval(verifyAuth, 300000) // Check every 5 minutes
+      return () => clearInterval(intervalId);
+      
+
+    }
+  }, [location.pathname, navigate]);
+  
+  
+  // Prevent rendering pages until authentication is verified
+  if (!isAuthVerified && location.pathname !== '/login') {
+    return <LoadingAnimated/>;
+  }
+
 
   return (
     <>
@@ -45,4 +97,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
