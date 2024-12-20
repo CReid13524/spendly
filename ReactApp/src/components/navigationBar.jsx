dba8b2198ef41493b3dd53df8f7ad85a6cbf4953
CReid13524 <christian.reid007@gmail.com>
2024-12-21 07:20:52 +1300

    Updated nav-panel design

--- ReactApp/src/components/navigationBar.jsx
+++ ReactApp/src/components/navigationBar.jsx
@@ -5,7 +5,13 @@ import { AiFillSetting } from "react-icons/ai";
 import { AiFillHome } from "react-icons/ai";
 import { BiSolidCategory } from "react-icons/bi";
 import { AiFillBook } from "react-icons/ai";
-import { RiBarChartFill } from "react-icons/ri";
+import { RiBarChart2Fill } from "react-icons/ri";
+
+import { AiOutlineSetting } from "react-icons/ai";
+import { AiOutlineHome } from "react-icons/ai";
+import { BiCategory } from "react-icons/bi";
+import { AiOutlineBook } from "react-icons/ai";
+import { RiBarChart2Line } from "react-icons/ri";
 
 function NavigationBar() {
   const location = useLocation(); 
@@ -14,11 +20,11 @@ function NavigationBar() {
     return (
       <div id="nav-container">
       <nav id="nav-panel">
-          <div className={`nav-item ${currentPath === "/" ? "active" : ""}`}><Link to="/" ><AiFillHome /></Link></div>
-          <div className={`nav-item ${currentPath === "/categories" ? "active" : ""}`}><Link to="/categories"><BiSolidCategory /></Link></div>
-          <div className={`nav-item ${currentPath === "/reconcile" ? "active" : ""}`}><Link to="/reconcile"><AiFillBook /></Link></div>
-          <div className={`nav-item ${currentPath === "/statistics" ? "active" : ""}`}><Link to="/statistics"><RiBarChartFill /></Link></div>
-          <div className={`nav-item ${currentPath === "/settings" ? "active" : ""}`}><Link to="/settings"><AiFillSetting /></Link></div>
+          <div className={`nav-item ${currentPath === "/" ? "active" : ""}`}><Link to="/" >{currentPath === "/" ? <AiOutlineHome/> : <AiFillHome />}</Link>Dashboard</div>
+          <div className={`nav-item ${currentPath === "/categories" ? "active" : ""}`}><Link to="/categories">{currentPath === "/categories" ? <BiCategory/> : <BiSolidCategory />}</Link>Categories</div>
+          <div className={`nav-item ${currentPath === "/reconcile" ? "active" : ""}`}><Link to="/reconcile">{currentPath === "/reconcile" ? <AiOutlineBook/> : <AiFillBook />}</Link>Reconcile</div>
+          <div className={`nav-item ${currentPath === "/statistics" ? "active" : ""}`}><Link to="/statistics">{currentPath === "/statistics" ? <RiBarChart2Line/> : <RiBarChart2Fill />}</Link>Statistics</div>
+          <div className={`nav-item ${currentPath === "/settings" ? "active" : ""}`}><Link to="/settings">{currentPath === "/settings" ? <AiOutlineSetting/> : <AiFillSetting />}</Link>Settings</div>
       </nav>
       </div>
     );
