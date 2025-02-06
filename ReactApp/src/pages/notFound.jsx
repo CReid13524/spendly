6613cf76757e65f26f3dd820df32f60af1d23343
CReid13524 <christian.reid007@gmail.com>
2025-02-07 11:38:55 +1300

    404 Page designed

--- ReactApp/src/pages/notFound.jsx
+++ ReactApp/src/pages/notFound.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import PageHeader from '../components/pageHeader'
 
 function notFound() {
   return (
-    <div>notFound</div>
+    <div id='notFound-container'>
+      <div id='notFound-page'>
+        <PageHeader/>
+        <div id="page-title">404 | Page not found</div>
+        <div id='page-notification'>The requested page could not be located. <a href='/'>Return to dashboard</a></div>
+      </div>
+    </div>
   )
 }
 
