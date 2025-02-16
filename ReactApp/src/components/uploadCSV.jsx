528ac4baebda36ff3120def012101b713b2de62f
CReid13524 <christian.reid007@gmail.com>
2025-02-17 09:30:39 +1300

    Added CSV type text

--- ReactApp/src/components/uploadCSV.jsx
+++ ReactApp/src/components/uploadCSV.jsx
@@ -40,8 +40,8 @@ function UploadCSV() {
     }
   }
   const options = [
-    {value:'anz', label:'ANZ', color:"#007aff", link:"https://digital.anz.co.nz/preauth/web/service/login", image:"https://www.anz.co.nz/content/dam/anzconz/media/logo/logo-anz.svg"},
-    {value:'kiwibank', label:'Kiwibank', color:"black", link:"https://www.ib.kiwibank.co.nz/login", image:"https://www.ib.kiwibank.co.nz/images/logo.png"},
+    {value:'anz', label:'ANZ', color:"#007aff", text:"Required Type: CSV", link:"https://digital.anz.co.nz/preauth/web/service/login", image:"https://www.anz.co.nz/content/dam/anzconz/media/logo/logo-anz.svg"},
+    {value:'kiwibank', label:'Kiwibank', color:"black", text:"Required Type: Full CSV",link:"https://www.ib.kiwibank.co.nz/login", image:"https://media.kiwibank.co.nz/media/images/Kiwibank_logo_white_250x250.width-500.webp"},
 
   ]
 
@@ -73,11 +73,14 @@ function UploadCSV() {
         <Select required options={options} closeMenuOnSelect placeholder="Select your bank" value={selectedOption} onChange={setSelectedOption}
           formatOptionLabel={e => (
             <div className='select-option'>
-              <img className='select-image' src={e.image} alt="" />
+              <img className='select-image' src={e.image} alt=""/>
               <span>{e.label}</span>
             </div>
           )} styles={customStyles} isLoading={isLoading} isDisabled={isLoading}/>
+          <div id="csv-external">
+          {selectedOption ? selectedOption.text : null}
           {selectedOption ? <a href={selectedOption.link} target="_blank" rel="noopener noreferrer"><FaExternalLinkAlt/></a> : null }
+          </div>
         <input type='file' accept='.csv' onChange={e => setFile(e.target.files[0])} required/>
         <button type='submit'>Upload File</button>
       </form>
